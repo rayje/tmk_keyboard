d41a7fef68b7a99f5d612d72dc756f2a2fcdaea2
Rayland <rayland.jeans@sendgrid.com>
2016-04-27 06:59:32 -0600

    simplify

--- keyboard/smallkey/keymap_smallkey.c
+++ keyboard/smallkey/keymap_smallkey.c
@@ -34,5 +34,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(KC_Q, KC_W, KC_A, KC_R), \ 
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-};
+const uint16_t PROGMEM fn_actions[] = {};
