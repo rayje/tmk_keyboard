e414f83f1956eb922c0e133c3f125418b192bed8
Rayland <rayland.jeans@sendgrid.com>
2016-04-26 22:45:47 -0600

    Add fourkey

---
+++ keyboard/fourkey/README.md
@@ -0,0 +1,10 @@
+# fourkey keyboard firmware
+
+The fourkey is a small mechanical keyboard firmware
+
+### Compiling
+
+```
+make teensy
+```
+
---
+++ keyboard/fourkey/keymap_fourkey.c
@@ -0,0 +1,8 @@
+#include "keymap_common.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    KEYMAP(KC_Q, KC_W, KC_A, KC_R), \ 
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+};
